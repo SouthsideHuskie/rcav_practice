a43ab17bd69e5d163b14e0392201fab2f3128cc2
SouthsideHuskie <r-mcguinness@kellogg.northwestern.edu>
2014-05-10 07:02:07 -0500

    Square Root Solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "calculations", :action => "home" })
-  get("/square_root/:number", { :controller => "calculations", :action => "sqrt" })
-  get("/payment/:interest_rate/:number_of_payments/:present_value", { :controller => "calculations", :action => "pmt" })
+  get("/home", { :controller => "calculation", :action => "home" })
+  get("/square_root/:number", { :controller => "calculation", :action => "square_root" })
+  get("/pmt/:interest_rate/:number_of_payments/:present_value", { :controller => "calculation", :action => "pmt" })
 end
