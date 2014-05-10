85122f6c6ae5d98aee4cb07999de2c5bf629d43d
SouthsideHuskie <r-mcguinness@kellogg.northwestern.edu>
2014-05-10 07:21:28 -0500

    Payment Solution

--- app/controllers/calculation_controller.rb
+++ app/controllers/calculation_controller.rb
@@ -10,5 +10,13 @@ class CalculationController < ApplicationController
   end
 
   def pmt
+
+    @interest_rate = params[:interest_rate].to_f
+    @interest_rate_perc = @interest_rate/"12".to_f/"100".to_f
+    @number_of_payments = params[:number_of_payments].to_f
+    @present_value = params[:present_value].to_f
+    @payment = (@present_value*(@interest_rate_perc*("1".to_f+@interest_rate_perc)**@number_of_payments)/(("1".to_f+@interest_rate_perc)**@number_of_payments -"1".to_f)).round(2)
+
+
   end
 end
